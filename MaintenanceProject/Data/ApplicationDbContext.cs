f9f8f1b95f0a70327eba9076a0d572b065ef7364
KareemMahafzah <KareemMahafzah@outlook.com>
2026-07-20 15:52:19 +0300

    0

---
+++ MaintenanceProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MaintenanceProject.Models;
+
+namespace MaintenanceProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<MaintenanceRequest> MaintenanceRequests { get; set; }
+    }
+}
---
+++ MaintenanceProject/MaintenanceRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MaintenanceProject.Models
+{
+    public class MaintenanceRequest
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string IssueDescription { get; set; }
+
+        [Required]
+        public string Priority { get; set; }
+
+        public DateTime DateSubmitted { get; set; } = DateTime.Now;
+    }
+}
